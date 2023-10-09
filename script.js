4f0d99e3e5f884610e8faa6884f59203a17655c3
Emiliano <emi.perez997@gmail.com>
2023-10-09 14:11:26 -0300

    [ADD] Menu hamburguesa

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const button = document.querySelector(".menu-button");
+  const navLinks = document.querySelector(".nav-links");
+
+  navLinks.classList.toggle("active");
+}
